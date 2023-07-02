db754e7ee8c1ec46b8731abdd00db58e3a153438
Luis Fernando <luisfborges2002@gmail.com>
2023-07-02 15:18:31 -0300

    :tada: New branch initiated.

---
+++ src/main/java/com/fernando/shorterurl/ShorterUrlApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.shorterurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShorterUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShorterUrlApplication.class, args);
+	}
+
+}
